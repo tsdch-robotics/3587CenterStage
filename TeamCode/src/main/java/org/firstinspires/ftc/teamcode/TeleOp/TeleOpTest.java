e9dcfdeab105a7a58590d298be688817cf7eb4a9
TSDCH Robotics (LabPC-3587) <LabPC-2827@robotics.tsdch>
2023-09-23 11:52:18 -0400

    hi

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleOpTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleOpTest.java
@@ -1,4 +1,41 @@
 package org.firstinspires.ftc.teamcode.TeleOp;
 
-public class TeleOpTest {
+import androidx.annotation.NonNull;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+@TeleOp()
+public class TeleOpTest extends OpMode {
+
+    DcMotor FL;
+    DcMotor FR;
+
+    DcMotor BL;
+
+    DcMotor BR;
+
+    @Override
+    public void init() {
+
+        FL = hardwareMap.get(DcMotor.class,  "FL");
+        FR = hardwareMap.get(DcMotor.class,  "FR");
+        BL = hardwareMap.get(DcMotor.class,  "BL");
+        BR = hardwareMap.get(DcMotor.class,  "BR");
+
+    }
+
+    @Override
+    public void loop() {
+
+        FL.setPower(gamepad1.left_stick_y);
+        BL.setPower(gamepad1.left_stick_y);
+        FR.setPower(gamepad1.right_stick_y);
+        BR.setPower(gamepad1.right_stick_y);
+
+    }
 }
+
+
--- libs/README.txt
+++ libs/README.txt
@@ -1,1 +1,2 @@
 Location of external libs
+
