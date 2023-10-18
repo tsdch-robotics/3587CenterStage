e408656b1b115cffab12d164509ad0de20ef0ac3
TSDCH Robotics (LabPC-3587) <LabPC-2827@robotics.tsdch>
2023-10-18 09:28:00 -0400

    hi

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/FieldCentric.java
@@ -0,0 +1,99 @@
+
+package org.firstinspires.ftc.teamcode.TeleOp;
+        import com.qualcomm.hardware.bosch.BNO055IMU;
+        import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+        import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+        import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+        import com.qualcomm.robotcore.hardware.DcMotor;
+        import com.qualcomm.robotcore.hardware.DcMotorSimple;
+        import com.qualcomm.robotcore.util.Range;
+@TeleOp(name = "FieldOriented", group = "TeleOp")
+public class FieldCentric extends OpMode {
+    private DcMotor frontLeftMotor;
+    private DcMotor frontRightMotor;
+    private DcMotor rearLeftMotor;
+    private DcMotor rearRightMotor;
+    private BNO055IMU imu; // Gyro sensor
+    private boolean gyroResetRequested = false;
+    @Override
+    public void init() {
+        frontLeftMotor = hardwareMap.dcMotor.get("FL");
+        frontRightMotor = hardwareMap.dcMotor.get("FR");
+        rearLeftMotor = hardwareMap.dcMotor.get("BL");
+        rearRightMotor = hardwareMap.dcMotor.get("BR");
+
+
+        frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        rearLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        // Initialize the gyro sensor
+        BNO055IMU.Parameters imuParams = new BNO055IMU.Parameters();
+        imuParams.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        imuParams.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        imuParams.calibrationDataFile = "BNO055IMUCalibration.json"; // Set this to your calibration file
+        imuParams.loggingEnabled = true;
+        imuParams.loggingTag = "IMU";
+        imuParams.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(imuParams);
+    }
+    @Override
+    public void loop() {
+        // Get joystick inputs from the gamepad
+        double drive = -gamepad1.left_stick_y;
+        double strafe = gamepad1.left_stick_x;
+        double rotate = gamepad1.right_stick_x;
+        if (gamepad1.a) {
+            gyroResetRequested = true;
+        }
+
+        // Perform gyro reset if requested
+        if (gyroResetRequested) {
+            resetGyro();
+            gyroResetRequested = false; // Reset the request flag
+        }
+
+        // Get the robot's heading from the gyro sensor
+        double heading = getHeading();
+        // Calculate the joystick inputs in the field-oriented frame of reference
+        double fieldDrive = drive * Math.cos(Math.toRadians(heading)) - strafe * Math.sin(Math.toRadians(heading));
+        double fieldStrafe = drive * Math.sin(Math.toRadians(heading)) + strafe * Math.cos(Math.toRadians(heading));
+        // Calculate motor powers for mecanum drive
+        double frontLeftPower = fieldDrive + fieldStrafe + rotate;
+        double frontRightPower = fieldDrive - fieldStrafe - rotate;
+        double rearLeftPower = fieldDrive - fieldStrafe + rotate;
+        double rearRightPower = fieldDrive + fieldStrafe - rotate;
+        // Ensure motor powers are within the valid range of -1 to 1
+        frontLeftPower = Range.clip(frontLeftPower, -1.0, 1.0);
+        frontRightPower = Range.clip(frontRightPower, -1.0, 1.0);
+        rearLeftPower = Range.clip(rearLeftPower, -1.0, 1.0);
+        rearRightPower = Range.clip(rearRightPower, -1.0, 1.0);
+        // Set motor powers
+        frontLeftMotor.setPower(frontLeftPower);
+        frontRightMotor.setPower(frontRightPower);
+        rearLeftMotor.setPower(rearLeftPower);
+        rearRightMotor.setPower(rearRightPower);
+        // Display motor powers on telemetry (optional)
+        telemetry.addData("Front Left Power", frontLeftPower);
+        telemetry.addData("Front Right Power", frontRightPower);
+        telemetry.addData("Rear Left Power", rearLeftPower);
+        telemetry.addData("Rear Right Power", rearRightPower);
+        telemetry.update();
+        // Update telemetry and control motors
+        telemetry.addData("Gyro Heading", heading);
+        telemetry.update();
+        }
+
+    private double getHeading() {
+        // Get the robot's heading from the gyro sensor
+        return imu.getAngularOrientation().firstAngle;
+    }
+    private void resetGyro() {
+        // Reset the gyro orientation to its initial state
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // Optional: Load a calibration file if available
+        imu.initialize(parameters);
+    }
+}
